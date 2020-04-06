88b92f9f5f374ed28493755fce23a8348c3a1cab
myoko175 <myoko175muscle@gmail.com>
2020-04-06 13:19:04 +0900

    20200406
    いろいろ

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,4 +1,10 @@
 class Machine < ApplicationRecord
   validates :category, :model, presence: true
   belongs_to :user
+  mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    return Machine.all unless search
+    Machine.where(['category LIKE(?) OR address LIKE(?)', "%#{search}%","%#{search}%"])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
-  resources :machines
+  resources :machines do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
 end
