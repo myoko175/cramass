f44b5515b6f01c0f7ec673d79f2d4bedb0985915
myoko175 <myoko175muscle@gmail.com>
2020-04-21 15:20:08 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :machines
+  validates :company, presence: true, uniqueness: true
 end
